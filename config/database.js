11e7655538219740a0cbc4585faa76a6fd3d30ea
andyvasquez-dev <andyvasquez.dev@gmail.com>
2020-09-27 00:27:33 -0400

    more to come

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://dbUser1:Vasquez123@cluster0.c2hbt.mongodb.net/savage?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'savage'
+};
